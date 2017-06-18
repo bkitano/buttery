a900b9eaba3891249996f429a17f52226ce733ab
Brian <brian.kitano@yale.edu>
2017-06-18 01:10:38 +0000

    connected to mlabs! Note: needed to update the local mongo CLI from 2.6 to 3.0 was the problem

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var port = 8080;
 var count = 0;
-
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
 
 // ------ MIDDLEWARE ------
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/node_modules')); 
+
+// mongo middleware
+var url = 'mongodb://brian:buttery@ds123182.mlab.com:23182/buttery';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to database");
+  db.close();
+});
 
 // ------ SITES -----------
 
@@ -34,11 +45,31 @@ io.on('connection', function(client) {
     
     client.on('order-from-client', function(data) {
         count++;
+        var d = new Date();
+        
         var order = {
+            'date' : d.toJSON(),
             'number' : count,
             'name' : data.name,
             'order' : data.order
         };
+        console.log(order);
+        // add to mongo database
+        
+        MongoClient.connect(url, function(err, db) {
+            if(err) {
+                console.log(err);
+            } else {
+                var collection = db.collection("orders");
+                collection.insertOne(order, function(err, result) {
+                    if(err){
+                        console.log(err);
+                    }
+                });
+            }
+            db.close();
+        });
+
        io.sockets.emit('order-to-client', order); 
     });
     
